6e0e6c9c9b726b7feedcfafc03cc45a34ac72562
kunal <kunalshah912@gmail.com>
2018-03-22 17:40:24 +0000

    temp changes

--- device.mk
+++ device.mk
@@ -317,8 +317,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-impl \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    power.msm8953
 
 # QMI
 PRODUCT_PACKAGES += \
